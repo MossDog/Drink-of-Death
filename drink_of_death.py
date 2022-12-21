2e3b9de98b505c536a850bbccd3d9baea6a72d47
MossDog <lukejohnmosshughes@gmail.com>
2022-12-21 01:41:27 +0000

    Part 1
    This is my complete solution for Part 1 of the Korean Drink of Death programming challenge

---
+++ drink_of_death.py
@@ -0,0 +1,46 @@
+import sys
+import pprint
+import random
+import numpy as np
+
+def main():
+
+    # parts = []
+    # for i in range(1, len(sys.argv)):
+        #parts.append(sys.argv[i])
+    parts = ["Luke", "David", "Conor", "Ryan", "James", "Keith", "Daniel", "Jordan", "Taylor"]
+    points = genPoints(parts)
+        
+            
+    pp = pprint.PrettyPrinter()
+    print("participants: ", end = "")
+    pp.pprint(parts)
+
+    print("Point location, Index -> Value: ", end = "")
+    pp.pprint(points)
+
+    for i in range(len(parts)):
+        print("{} | {} points to {}".format(i, parts[i], parts[points[i]]))
+
+    print("Please choose a leader! (0 - {})".format(len(parts)-1))
+    curr = int(input())
+    print("{}! You are the leader!\nHow many steps would you like to take?".format(parts[curr]))
+    n = int(input())
+    for i in range(n):
+        print("{} | {} -> {}".format(i, parts[curr], parts[points[curr]]))
+        curr = points[curr]
+    print("{}, my brother in christ, you must consume the liquid! 0_0".format(parts[curr]))
+def genPoints(parts):
+    points = []
+    nums = np.arange(len(parts))
+    for i in range(len(parts)):
+
+        test = [n for n in nums if n != i and n not in points]
+        if len(test)==0:
+            points = genPoints(parts)
+        points.append(random.choice(test))
+    return points
+
+
+if __name__ == '__main__':
+    main()
