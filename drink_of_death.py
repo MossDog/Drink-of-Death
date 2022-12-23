f6e57290efd6efc5ecaf622efff495df76ffbd05
MossDog <lukejohnmosshughes@gmail.com>
2022-12-23 01:20:22 +0000

    Part 3
    This is my complete solution for Part 3 of the Korean Drink of Death programming challenge and also my final solution to the challenge at large, the print statements and code was cleaned up for this part as well as the addition feature for counting loops etc

--- drink_of_death.py
+++ drink_of_death.py
@@ -1,44 +1,85 @@
+'''
+Author: Luke Moss 'Mossdog' Hughes
+Note: I hope nobody expects comments on this abstract mess B)
+'''
+
 import sys
 import random
 import numpy as np
 
 def main():
-    # parts = []
-    # for i in range(1, len(sys.argv)):
-        #parts.append(sys.argv[i])
-    parts = ["Luke", "David", "Conor", "Ryan", "James", "Keith", "Daniel", "Jordan", "Taylor"]
+    parts = []
+    for i in range(1, len(sys.argv)):
+        parts.append(sys.argv[i])
+    #parts = ["Luke", "David", "Conor", "Ryan", "James", "Keith", "Daniel", "Jordan", "Taylor"]
     points = genPoints(parts)
 
     # Display who's pointing to who
+    print("PLAYERS")
     for i in range(len(parts)):
-        print("{} | {} points to {}".format(i, parts[i], parts[points[i]]))
+        print(f"{i} | {parts[i]} points to {parts[points[i]]}")
 
-    print("Please choose a leader! (0 - {})".format(len(parts)-1))
+    print(f"\nPlease choose a leader! (0 - {len(parts)-1})")
     leader = int(input()) # Store leader in curr
     
     runGame(parts, points, leader)
+    findLoops(parts, points)
+
+
+def findLoops(parts, points):
+    loops = []
+    for i in range(len(parts)):
+        curr = i
+        loop = []
+
+        for j in range(len(parts)):
+            loop.append(parts[curr])
+            curr = points[curr]
+
+            if curr == i:
+                stored = None
+                
+                for iLoop in loops:
+                    if loop[0] in iLoop:
+                        stored = True
+
+                if not stored:
+                    loops.append(loop)
+
+                break
+
+    print(f"\nThere are {len(loops)} loops, the longest is {len(max(loops, key=len))} long and is shown below\n{max(loops, key=len)}")
+
+    if len(loops)>1:
+        print("\nOther loops")
+
+        for loop in loops:
+            if loop != max(loops, key=len):
+                print(loop)
 
 
 def runGame(parts, points, leader):
     superDict = {1:'\u02e2\u1d57', 2:'\u207F;\u1D48', 3:'\u02B3\u1d48'} # 1:st, 2:nd, 3:rd
+    thStr = '\u1D57\u02B0'
     loop = None
     curr = leader
 
-    print("{}! You are the leader!\nHow many steps would you like to take?".format(parts[curr]))
+    print(f"\n{parts[curr]}! You are the leader!\nHow many steps would you like to take?")
     n = int(input())
 
+    print("\nRUNNING...")
     for i in range(n):
-        print("{} | {} -> {}".format(i, parts[curr], parts[points[curr]]))
+        print(f"{i+1} | {parts[curr]} -> {parts[points[curr]]}")
         curr = points[curr]
 
-        if(curr == leader and loop == None):
-            loop = i
+        if curr == leader and loop == None:
+            loop = i+1
 
-    print("MFW {}, my brother in christ, must consume the Death Juice! 0_0".format(parts[curr]))
+    print(f"\nMy brother in christ, {parts[curr]}, must consume the Death Juice!")
 
     if loop != None:
-        # \u1D57\u02B0 is the default value (st) which is used for all numbers from 10-20
-        print("A loop was entered on the {}{} step".format(loop, superDict.get(loop if (loop<20) else (loop%10), '\u1D57\u02B0')))
+        # thStr is the default value (st) which is used for all numbers from 10-20
+        print(f"\nA loop was entered on the {loop}{superDict.get(loop if loop<20 else loop%10, thStr)} step")
 
 
 def genPoints(parts):
@@ -52,7 +93,7 @@ def genPoints(parts):
             points = genPoints(parts)
 
         points.append(random.choice(test))
-        
+
     return points
 
 
